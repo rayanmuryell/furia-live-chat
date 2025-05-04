bb2c8ea936ba7d8dff38d0703bbf781fd5729df2
Rayan Muryell <rayan_muryell@hotmail.com>
2025-05-04 17:08:33 -0300

    changed 'a' to 'link'

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Header({ onLoginClick }: { onLoginClick: () => void }) {
     return (
         <header className="sticky top-0 bg-black flex items-center p-4 border-b border-gray-800 z-10">
@@ -11,15 +13,15 @@ export default function Header({ onLoginClick }: { onLoginClick: () => void }) {
                 src="https://platform.twitter.com/widgets.js"
             ></script>
             <div className="flex space-x-4">
-                <a className="btn btn-ghost text-sm">FURIA eSports</a>
-                <a href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Página Inicial</a>
-                <a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" onClick={onLoginClick}>Transmissões Ao Vivo
+                <Link className="btn btn-ghost text-sm" href="#">FURIA eSports</Link>
+                <Link href="/" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white" aria-current="page">Página Inicial</Link>
+                <Link href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white" onClick={onLoginClick}>Transmissões Ao Vivo
                     <div className="ml-2 inline-grid *:[grid-area:1/1]">
                         <div className="status status-success animate-ping"></div>
                         <div className="status status-success"></div>
                     </div>
-                </a>
-                <a href="/players" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jogadores</a>
+                </Link>
+                <Link href="/players" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Jogadores</Link>
             </div>
         </header>
     )
